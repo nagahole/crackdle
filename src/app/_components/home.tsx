080568726c90afe181259db3cecfc23f865d2ba7
Rick Wang <rickwang121@gmail.com>
2025-11-10 16:42:25 +1300

    doc: comments

--- src/app/_components/home.tsx
+++ src/app/_components/home.tsx
@@ -13,18 +13,19 @@ export function Home({ user }: { user: User; }) {
         <main className="flex bg-black min-h-screen flex-col items-center justify-center gap-4">
             <Logo/>
             <Subtitle/>
+
+            {/* logged in as johndoe@email.com */}
             <div className="text-sm text-gray-500">
                 Logged in as {user.email ?? user.id}
             </div>
+
             <h2 className="text-2xl text-white font-semibold">Find a game</h2>
+
             <button className="rounded bg-blue-600 px-4 py-2 text-white">
                 Quick Match
             </button>
-            {/* logout button */}
-            <button
-                className="rounded bg-red-600 px-4 py-2 text-white"
-                onClick={async () => await supabase.auth.signOut()}
-            >
+
+            <button className="rounded bg-red-600 px-4 py-2 text-white" onClick={async () => await supabase.auth.signOut()}>
                 Logout
             </button>
         </main>
